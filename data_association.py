d5b4334b688e1470ffd6ae3bdb0181570dbec3a6
Fatemeh Karimi Nejadasl <fkariminejadasl@gmail.com>
2022-10-25 09:19:55 +0200

    Some refactoring and moving functions around.

--- data_association.py
+++ data_association.py
@@ -12,35 +12,26 @@ stopped_track_length = 50
 
 result_folder = Path("/home/fatemeh/results/dataset1")
 data_folder = Path("/home/fatemeh/data/dataset1")
-track_folder = (
-    data_folder / "cam1_labels/cam1_labels"
-)  # cam1_labels/cam1_labels, cam2_labels/cam2_labels
-filename_fixpart = "12_07_22_1_C_GH040468_1_cam1_rect"  # 12_07_22_1_C_GH040468_1_cam1_rect, 12_07_22_1_D_GH040468_1_cam2_rect
-vc = cv2.VideoCapture((data_folder / f"{filename_fixpart}.mp4").as_posix())
-vc.isOpened()
+det_folder1 = data_folder / "cam1_labels/cam1_labels"
+det_folder2 = data_folder / "cam2_labels/cam2_labels"
+filename_fixpart1 = "12_07_22_1_C_GH040468_1_cam1_rect"
+filename_fixpart2 = "12_07_22_1_D_GH040468_1_cam2_rect"
+vc1 = cv2.VideoCapture((data_folder / f"{filename_fixpart1}.mp4").as_posix())
+vc2 = cv2.VideoCapture((data_folder / f"{filename_fixpart2}.mp4").as_posix())
 
 
-# visualize detection as video
-height = int(vc.get(cv2.CAP_PROP_FRAME_HEIGHT))
-width = int(vc.get(cv2.CAP_PROP_FRAME_WIDTH))
-total_no_frames = int(vc.get(cv2.CAP_PROP_FRAME_COUNT))
-fps = vc.get(cv2.CAP_PROP_FPS)
+def get_video_parameters(vc: cv2.VideoCapture):
+    if vc.isOpened():
+        height = int(vc.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        width = int(vc.get(cv2.CAP_PROP_FRAME_WIDTH))
+        total_no_frames = int(vc.get(cv2.CAP_PROP_FRAME_COUNT))
+        fps = vc.get(cv2.CAP_PROP_FPS)
+        return height, width, total_no_frames, fps
+    else:
+        return
 
 
-def draw_matches(frame1, frame2, matches1, matches2):
-    _, ax1 = plt.subplots(1, 1)
-    _, ax2 = plt.subplots(1, 1)
-    ax1.imshow(frame1[..., ::-1])
-    ax2.imshow(frame2[..., ::-1])
-    ax1.set_xlim(1400, 2100)
-    ax1.set_ylim(1200, 700)
-    ax2.set_xlim(1400, 2100)
-    ax2.set_ylim(1200, 700)
-
-    for match1, match2 in zip(matches1, matches2):
-        ax1.plot([match1.x, match2.x], [match1.y, match2.y], "*-", color=(0, 0, 1))
-        ax2.plot([match1.x, match2.x], [match1.y, match2.y], "*-", color=(0, 0, 1))
-    plt.show(block=False)
+height, width, total_no_frames, fps = get_video_parameters(vc1)
 
 
 class Status(enum.Enum):
@@ -74,7 +65,7 @@ class Track:
     status: Status
 
 
-def get_detections(det_path, frame_number) -> list[Detection]:
+def get_detections(det_path, frame_number, width: int, height: int) -> list[Detection]:
     detections = np.loadtxt(det_path)
     return [
         Detection(
@@ -97,7 +88,7 @@ def match_two_detection_sets(dets1, dets2):
     return row_ind, col_ind
 
 
-def _make_a_new_track(coords, frameids, flow, status) -> Track:
+def _make_a_new_track(coords, frameids, flow, track_id, status) -> Track:
     color = tuple(np.random.rand(3).astype(np.float16))
     pred = Point(x=flow.x + coords[-1].x, y=flow.y + coords[-1].y)
     predicted_loc = Detection(
@@ -121,184 +112,135 @@ def _get_common_flow(flows):
     return common_flow
 
 
-# initiate track
-# ===============
-frame_number1 = 1
-frame_number2 = 2
-det_path1 = track_folder / f"{filename_fixpart}_{frame_number1}.txt"
-det_path2 = track_folder / f"{filename_fixpart}_{frame_number2}.txt"
-dets1 = get_detections(det_path1, frame_number1)
-dets2 = get_detections(det_path2, frame_number2)
-ids1, ids2 = match_two_detection_sets(dets1, dets2)
-tracks = {}
-track_id = 0
-# matched tracks
-flows = [Point(x=0.0, y=0.0)]
-for id1, id2 in zip(ids1, ids2):
-    dist = np.linalg.norm([dets1[id1].x - dets2[id2].x, dets1[id1].y - dets2[id2].y])
-    if dist < accepted_flow_length:
-        coords = [dets1[id1], dets2[id2]]
-        frameids = [frame_number1, frame_number2]
-        flow = Point(x=coords[-1].x - coords[-2].x, y=coords[-1].y - coords[-2].y)
-        flow_length = np.linalg.norm([flow.x, flow.y])
-        if flow_length < accepted_flow_length:
-            flows.append(flow)
-        tracks[track_id] = _make_a_new_track(coords, frameids, flow, Status.Tracked)
-        track_id += 1
-    else:
-        common_flow = _get_common_flow(flows)
-        tracks[track_id] = _make_a_new_track(
-            [dets1[id1]], [frame_number1], common_flow, Status.NewTrack
-        )
-        track_id += 1
-        tracks[track_id] = _make_a_new_track(
-            [dets2[id2]], [frame_number2], common_flow, Status.NewTrack
-        )
-        track_id += 1
-
-common_flow = _get_common_flow(flows)
-
-# unmatched tracks: frame1
-diff_ids = set(range(len(dets1))).difference(set(ids1))
-for id in diff_ids:
-    coords = [dets1[id]]
-    frameids = [frame_number1]
-    flow = Point(x=common_flow.x * 2, y=common_flow * 2)
-
-    tracks[track_id] = _make_a_new_track(coords, frameids, flow, Status.NewTrack)
-    track_id += 1
-# unmatched tracks: frame2
-diff_ids = set(range(len(dets2))).difference(set(ids2))
-for id in diff_ids:
-    coords = [dets2[id]]
-    frameids = [frame_number2]
-    flow = common_flow
-
-    tracks[track_id] = _make_a_new_track(coords, frameids, flow, Status.NewTrack)
-    track_id += 1
-
-# start track
-# ===========
-for frame_number in range(3, 681):
-    det_path = track_folder / f"{filename_fixpart}_{frame_number}.txt"
-    dets = get_detections(det_path, frame_number)
-    pred_dets = [
-        track.predicted_loc
-        for _, track in tracks.items()
-        if track.status != Status.Stoped
-    ]
-    ids1, ids2 = match_two_detection_sets(pred_dets, dets)
-
+def compute_tracks(det_folder: Path, filename_fixpart: str, width: int, height: int):
+    # initiate track
+    # ===============
+    frame_number1 = 1
+    frame_number2 = 2
+    det_path1 = det_folder / f"{filename_fixpart}_{frame_number1}.txt"
+    det_path2 = det_folder / f"{filename_fixpart}_{frame_number2}.txt"
+    dets1 = get_detections(det_path1, frame_number1, width, height)
+    dets2 = get_detections(det_path2, frame_number2, width, height)
+    ids1, ids2 = match_two_detection_sets(dets1, dets2)
+    tracks = {}
+    track_id = 0
+    # matched tracks
     flows = [Point(x=0.0, y=0.0)]
     for id1, id2 in zip(ids1, ids2):
-        current_track_id = pred_dets[id1].id
-        track = tracks[current_track_id]
-        # kill tracks that are not tracked for a while
-        if frame_number - track.frameids[-1] > stopped_track_length:
-            track.status = Status.Stoped
-        else:
-            dist = np.linalg.norm(
-                [pred_dets[id1].x - dets[id2].x, pred_dets[id1].y - dets[id2].y]
+        dist = np.linalg.norm(
+            [dets1[id1].x - dets2[id2].x, dets1[id1].y - dets2[id2].y]
+        )
+        if dist < accepted_flow_length:
+            coords = [dets1[id1], dets2[id2]]
+            frameids = [frame_number1, frame_number2]
+            flow = Point(x=coords[-1].x - coords[-2].x, y=coords[-1].y - coords[-2].y)
+            flow_length = np.linalg.norm([flow.x, flow.y])
+            if flow_length < accepted_flow_length:
+                flows.append(flow)
+            tracks[track_id] = _make_a_new_track(
+                coords, frameids, flow, track_id, Status.Tracked
             )
-            if dist < accepted_flow_length:
-                track.coords.append(dets[id2])
-                track.frameids.append(frame_number)
-                flow = Point(
-                    x=track.coords[-1].x - track.coords[-2].x,
-                    y=track.coords[-1].y - track.coords[-2].y,
-                )
-                flow_length = np.linalg.norm([flow.x, flow.y])
-                if flow_length < accepted_flow_length:
-                    flows.append(flow)
-                pred = Point(
-                    x=flow.x + track.coords[-1].x, y=flow.y + track.coords[-1].y
-                )
-                track.predicted_loc.x = pred.x
-                track.predicted_loc.y = pred.y
-                track.status = Status.Tracked
-            else:
-                track.predicted_loc.x = common_flow.x + track.predicted_loc.x
-                track.predicted_loc.y = common_flow.y + track.predicted_loc.y
-                if track.status != Status.NewTrack:
-                    track.status = Status.Untracked
+            track_id += 1
+        else:
+            common_flow = _get_common_flow(flows)
+            tracks[track_id] = _make_a_new_track(
+                [dets1[id1]], [frame_number1], common_flow, track_id, Status.NewTrack
+            )
+            track_id += 1
+            tracks[track_id] = _make_a_new_track(
+                [dets2[id2]], [frame_number2], common_flow, track_id, Status.NewTrack
+            )
+            track_id += 1
+
     common_flow = _get_common_flow(flows)
 
     # unmatched tracks: frame1
-    diff_ids = set(range(len(pred_dets))).difference(set(ids1))
+    diff_ids = set(range(len(dets1))).difference(set(ids1))
     for id in diff_ids:
-        current_track_id = pred_dets[id].id
-        track = tracks[current_track_id]
-        track.predicted_loc.x += common_flow.x
-        track.predicted_loc.y += common_flow.y
-        if track.status != Status.NewTrack:
-            track.status = Status.Untracked
-
-    # unmatched tracks: frame2
-    diff_ids = set(range(len(dets))).difference(set(ids2))
-    for id in diff_ids:
-        coords = [dets2[id]]
-        frameids = [frame_number]
+        coords = [dets1[id]]
+        frameids = [frame_number1]
+        flow = Point(x=common_flow.x * 2, y=common_flow * 2)
 
         tracks[track_id] = _make_a_new_track(
-            coords, frameids, common_flow, Status.NewTrack
+            coords, frameids, flow, track_id, Status.NewTrack
+        )
+        track_id += 1
+    # unmatched tracks: frame2
+    diff_ids = set(range(len(dets2))).difference(set(ids2))
+    for id in diff_ids:
+        coords = [dets2[id]]
+        frameids = [frame_number2]
+        flow = common_flow
+
+        tracks[track_id] = _make_a_new_track(
+            coords, frameids, flow, track_id, Status.NewTrack
         )
         track_id += 1
 
-# visualize tracks
-vc.set(cv2.CAP_PROP_POS_FRAMES, 0)
-_, frame1 = vc.read()
-plt.figure()
-plt.imshow(frame1[..., ::-1])
-for _, track in tracks.items():
-    plt.plot(
-        [det.x for det in track.coords],
-        [det.y for det in track.coords],
-        "*-",
-        color=track.color,
-    )
-plt.show(block=False)
+    # start track
+    # ===========
+    for frame_number in range(3, 681):
+        det_path = det_folder / f"{filename_fixpart}_{frame_number}.txt"
+        dets = get_detections(det_path, frame_number, width, height)
+        pred_dets = [
+            track.predicted_loc
+            for _, track in tracks.items()
+            if track.status != Status.Stoped
+        ]
+        ids1, ids2 = match_two_detection_sets(pred_dets, dets)
 
+        flows = [Point(x=0.0, y=0.0)]
+        for id1, id2 in zip(ids1, ids2):
+            current_track_id = pred_dets[id1].id
+            track = tracks[current_track_id]
+            # kill tracks that are not tracked for a while
+            if frame_number - track.frameids[-1] > stopped_track_length:
+                track.status = Status.Stoped
+            else:
+                dist = np.linalg.norm(
+                    [pred_dets[id1].x - dets[id2].x, pred_dets[id1].y - dets[id2].y]
+                )
+                if dist < accepted_flow_length:
+                    track.coords.append(dets[id2])
+                    track.frameids.append(frame_number)
+                    flow = Point(
+                        x=track.coords[-1].x - track.coords[-2].x,
+                        y=track.coords[-1].y - track.coords[-2].y,
+                    )
+                    flow_length = np.linalg.norm([flow.x, flow.y])
+                    if flow_length < accepted_flow_length:
+                        flows.append(flow)
+                    pred = Point(
+                        x=flow.x + track.coords[-1].x, y=flow.y + track.coords[-1].y
+                    )
+                    track.predicted_loc.x = pred.x
+                    track.predicted_loc.y = pred.y
+                    track.status = Status.Tracked
+                else:
+                    track.predicted_loc.x = common_flow.x + track.predicted_loc.x
+                    track.predicted_loc.y = common_flow.y + track.predicted_loc.y
+                    if track.status != Status.NewTrack:
+                        track.status = Status.Untracked
+        common_flow = _get_common_flow(flows)
 
-# visualize tracks on two frames
-vc.set(cv2.CAP_PROP_POS_FRAMES, frame_number1 - 1)
-_, frame1 = vc.read()
-vc.set(cv2.CAP_PROP_POS_FRAMES, frame_number - 1)
-_, frame2 = vc.read()
-_, (ax1, ax2) = plt.subplots(2, 1, sharex=True, sharey=True)
-ax1.imshow(frame1[..., ::-1])
-ax2.imshow(frame2[..., ::-1])
-for _, track in tracks.items():
-    for id in track.frameids:
-        if id == frame_number1:
-            ax1.plot([track.coords[0].x], [track.coords[0].y], "*", color=track.color)
-        if id == frame_number:
-            ax2.plot([track.coords[-1].x], [track.coords[-1].y], "*", color=track.color)
-plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
-plt.show(block=False)
+        # unmatched tracks: frame1
+        diff_ids = set(range(len(pred_dets))).difference(set(ids1))
+        for id in diff_ids:
+            current_track_id = pred_dets[id].id
+            track = tracks[current_track_id]
+            track.predicted_loc.x += common_flow.x
+            track.predicted_loc.y += common_flow.y
+            if track.status != Status.NewTrack:
+                track.status = Status.Untracked
 
-# roughly up to 50 frames they can track
-# c = 0
-# plt.figure()
-# for k, track in tracks.items():
-#     if track.status == Status.Tracked:
-#         c += 1
-#         print(f"{k},{len(track.frameids)}")
-#         plt.plot(track.frameids,"*-",label=str(k))
-# print(f"{c}")
+        # unmatched tracks: frame2
+        diff_ids = set(range(len(dets))).difference(set(ids2))
+        for id in diff_ids:
+            coords = [dets2[id]]
+            frameids = [frame_number]
 
-# frameid-y axis
-def plot_frameid_y(tracks, status, legned=False):
-    _, ax = plt.subplots(1, 1)
-    for k, track in tracks.items():
-        if track.status == status:
-            ax.plot(
-                track.frameids,
-                [coord.y for coord in track.coords],
-                "*-",
-                color=track.color,
-                label=str(k),
+            tracks[track_id] = _make_a_new_track(
+                coords, frameids, common_flow, track_id, Status.NewTrack
             )
-            print(f"{k}, {track.status}")
-    if legned == True:
-        ax.legend()
-    plt.show(block=False)
+            track_id += 1
+    return tracks
--- visualize.py
+++ visualize.py
@@ -3,99 +3,170 @@ from pathlib import Path
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy.optimize import linear_sum_assignment
-
-data_folder = Path("/home/fatemeh/data/dataset1")
-result_folder = Path("/home/fatemeh/results/dataset1")
-track_folder = Path("/home/fatemeh/data/dataset1/cam1_labels/cam1_labels")
-vc = cv2.VideoCapture(
-    (data_folder / "12_07_22_1_C_GH040468_1_cam1_rect.mp4").as_posix()
-)
-vc.isOpened()
-
-# visualize detection as video
-height = int(vc.get(cv2.CAP_PROP_FRAME_HEIGHT))
-width = int(vc.get(cv2.CAP_PROP_FRAME_WIDTH))
-total_no_frames = int(vc.get(cv2.CAP_PROP_FRAME_COUNT))
-fps = vc.get(cv2.CAP_PROP_FPS)
-
-fourcc = cv2.VideoWriter_fourcc(*"XVID")
-out = cv2.VideoWriter(
-    (result_folder / "output.avi").as_posix(), fourcc, fps, (width, height)
-)
-
-for frame_number in range(1, total_no_frames + 1):
-    vc.set(cv2.CAP_PROP_POS_FRAMES, frame_number - 1)
-    rval, frame = vc.read()
-
-    track_path = track_folder / f"12_07_22_1_C_GH040468_1_cam1_rect_{frame_number}.txt"
-    track = np.loadtxt(track_path)
-
-    Xs = np.int64(np.round(track[:, 1] * width))
-    Ys = np.int64(np.round(track[:, 2] * height))
-    Ws = np.int64(np.round(track[:, 3] * width))
-    Hs = np.int64(np.round(track[:, 4] * height))
-    # # show as bounding boxes
-    # for x, y, w, h in zip(Xs, Ys, Ws, Hs):
-    #     w2 = int(w/2)
-    #     h2  = int(h/2)
-    #     color = tuple(int(i) for i in np.random.randint(0, 255, (3,)))
-    #     cv2.rectangle(frame, (x-w2,y-h2), (x+w2,y+h2), color=color, thickness=2)
-
-    # show as thick points
-    for i in range(6):
-        for j in range(6):
-            frame[Ys + i, Xs + j, :] = np.array([0, 0, 255])
-
-    out.write(frame)
-out.release()
+from data_association import Point, get_detections, get_video_parameters
 
 
-# visualize detections in frames
-for frame_number in range(1, total_no_frames + 1, 90):
-    vc.set(cv2.CAP_PROP_POS_FRAMES, frame_number - 1)
-    rval, frame = vc.read()
+def visualize_detections_in_video(
+    det_folder,
+    vc,
+    output_video_file,
+    color=(0, 0, 255),
+):
+    height, width, total_no_frames, fps = get_video_parameters(vc)
 
-    track_path = track_folder / f"12_07_22_1_C_GH040468_1_cam1_rect_{frame_number}.txt"
-    track = np.loadtxt(track_path)
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    out = cv2.VideoWriter(output_video_file.as_posix(), fourcc, fps, (width, height))
 
-    plt.figure()
-    plt.imshow(frame[..., ::-1])
-    plt.plot(track[:, 1] * frame.shape[1], track[:, 2] * frame.shape[0], "r*")
-    plt.show(block=False)
+    for frame_number in range(1, total_no_frames + 1):
+        vc.set(cv2.CAP_PROP_POS_FRAMES, frame_number - 1)
+        _, frame = vc.read()
 
-vc.release()
+        det_path = det_folder / f"12_07_22_1_C_GH040468_1_cam1_rect_{frame_number}.txt"
+        dets = get_detections(det_path, frame_number, width, height)
 
-
-# visualize tracks as video
-fourcc = cv2.VideoWriter_fourcc(*"XVID")
-out = cv2.VideoWriter(
-    (result_folder / "track_cam1.avi").as_posix(),
-    fourcc,
-    fps,
-    (700, 500),  # (width, height)
-)
-for frame_number in range(1, total_no_frames + 1):
-    vc.set(cv2.CAP_PROP_POS_FRAMES, frame_number - 1)
-    _, frame = vc.read()
-
-    for _, track in tracks.items():
-        if frame_number in track.frameids:
-            color = tuple(int(round(c * 255)) for c in track.color)
-            idx = np.where(np.array(track.frameids) == frame_number)[0][0]
-            coord = track.coords[idx]
+        for coord in dets:
             w2 = int(coord.w / 2)
             h2 = int(coord.h / 2)
             cv2.rectangle(
                 frame,
-                (coord.x - w2, coord.y - h2),
-                (coord.x + w2, coord.y + h2),
+                (int(coord.x) - w2, int(coord.y) - h2),
+                (int(coord.x) + w2, int(coord.y) + h2),
                 color=color,
-                thickness=2,
+                thickness=1,
             )
             # show as thick points
             # for i in range(6):
             #     for j in range(6):
-            #         frame[coord.y + i, coord.x + j, :] = color
-    out.write(frame[700:1200, 1400:2100, :])  # cam1: 1400:2100; cam2: 1000:1700
-out.release()
+            #         frame[int(coord.y) + i, int(coord.x) + j, :] = np.array(color)
+        out.write(frame)
+    out.release()
+
+
+def draw_matches(frame1, frame2, matches1, matches2):
+    _, ax1 = plt.subplots(1, 1)
+    _, ax2 = plt.subplots(1, 1)
+    ax1.imshow(frame1[..., ::-1])
+    ax2.imshow(frame2[..., ::-1])
+    ax1.set_xlim(1400, 2100)
+    ax1.set_ylim(1200, 700)
+    ax2.set_xlim(1000, 1700)
+    ax2.set_ylim(1200, 700)  # cam1: 1400:2100; cam2: 1000:1700
+
+    for match1, match2 in zip(matches1, matches2):
+        ax1.plot([match1.x, match2.x], [match1.y, match2.y], "*-", color=(0, 0, 1))
+        ax2.plot([match1.x, match2.x], [match1.y, match2.y], "*-", color=(0, 0, 1))
+    plt.show(block=False)
+
+
+def visualize_tracks_on_a_frame(tracks, vc, frame_number=1):
+    vc.set(cv2.CAP_PROP_POS_FRAMES, frame_number - 1)
+    _, frame1 = vc.read()
+    plt.figure()
+    plt.imshow(frame1[..., ::-1])
+    for _, track in tracks.items():
+        plt.plot(
+            [det.x for det in track.coords],
+            [det.y for det in track.coords],
+            "*-",
+            color=track.color,
+        )
+    plt.show(block=False)
+
+
+def visualize_tracks_on_two_frames(tracks, vc, frame_number1, frame_number2):
+    vc.set(cv2.CAP_PROP_POS_FRAMES, frame_number1 - 1)
+    _, frame1 = vc.read()
+    vc.set(cv2.CAP_PROP_POS_FRAMES, frame_number2 - 1)
+    _, frame2 = vc.read()
+    _, (ax1, ax2) = plt.subplots(2, 1, sharex=True, sharey=True)
+    ax1.imshow(frame1[..., ::-1])
+    ax2.imshow(frame2[..., ::-1])
+    for _, track in tracks.items():
+        for id in track.frameids:
+            if id == frame_number1:
+                ax1.plot(
+                    [track.coords[0].x], [track.coords[0].y], "*", color=track.color
+                )
+            if id == frame_number2:
+                ax2.plot(
+                    [track.coords[-1].x], [track.coords[-1].y], "*", color=track.color
+                )
+    plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+    plt.show(block=False)
+
+
+# roughly up to 50 frames they can be tracked
+# c = 0
+# plt.figure()
+# for k, track in tracks.items():
+#     if track.status == Status.Tracked:
+#         c += 1
+#         print(f"{k},{len(track.frameids)}")
+#         plt.plot(track.frameids,"*-",label=str(k))
+# print(f"{c}")
+
+# frameid-y axis
+def plot_frameid_y(tracks, status, legned=False):
+    _, ax = plt.subplots(1, 1)
+    for k, track in tracks.items():
+        if track.status == status:
+            ax.plot(
+                track.frameids,
+                [coord.y for coord in track.coords],
+                "*-",
+                color=track.color,
+                label=str(k),
+            )
+            print(f"{k}, {track.status}")
+    if legned == True:
+        ax.legend()
+    plt.show(block=False)
+
+
+def visualize_tracks_in_video(
+    tracks,
+    vc,
+    output_video_file,
+    focus_point=Point(1400, 700),
+    out_width=700,
+    out_height=500,
+):
+    _, _, total_no_frames, fps = get_video_parameters(vc)
+
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    out = cv2.VideoWriter(
+        output_video_file.as_posix(),
+        fourcc,
+        fps,
+        (out_width, out_height),  # (width, height)
+    )
+    for frame_number in range(1, total_no_frames + 1):
+        vc.set(cv2.CAP_PROP_POS_FRAMES, frame_number - 1)
+        _, frame = vc.read()
+
+        for _, track in tracks.items():
+            if frame_number in track.frameids:
+                color = tuple(int(round(c * 255)) for c in track.color)
+                idx = np.where(np.array(track.frameids) == frame_number)[0][0]
+                coord = track.coords[idx]
+                w2 = int(coord.w / 2)
+                h2 = int(coord.h / 2)
+                cv2.rectangle(
+                    frame,
+                    (int(coord.x) - w2, int(coord.y) - h2),
+                    (int(coord.x) + w2, int(coord.y) + h2),
+                    color=color,
+                    thickness=1,
+                )
+                # show as thick points
+                # for i in range(6):
+                #     for j in range(6):
+                #         frame[coord.y + i, coord.x + j, :] = color
+        out.write(
+            frame[
+                int(focus_point.y) : int(focus_point.y) + out_height,
+                int(focus_point.x) : int(focus_point.x) + out_width,
+                :,
+            ]
+        )  # cam1: 1400:2100; cam2: 1000:1700, height: 700:1200
+    out.release()
