b84d55cb900717f3933e87bf94ceb61c58f24b0e
Fatemeh Karimi Nejadasl <fkariminejadasl@gmail.com>
2024-04-29 17:14:51 +0200

    bug resolve: in interpolation; merge by speed

--- tracking/postprocess.py
+++ tracking/postprocess.py
@@ -18,24 +18,24 @@ def get_matching_frames_between_tracks(tracks1, tracks2, track_id1, track_id2):
     return track1, track2
 
 
-def correct_outliers(track1, track2, max_disp=10):
+def correct_outliers(track, max_disp=10):
     """
-    example for track 7:8 frame 1400 has issues
-    track1[np.arange(1396,1404+1),7] = np.interp(np.arange(1396,1404+1), [1396,1404], [track1[1396,7], track1[1404,7]])
-    track1[np.arange(1396,1404+1),8] = np.interp(np.arange(1396,1404+1), [1396,1404], [track1[1396,8], track1[1404,8]])
+    example for gtrack1 7:8 frame 1400 has issues (129_1)
+    track[np.arange(1396,1404+1),7] = np.interp(np.arange(1396,1404+1), [1396,1404], [track[1396,7], track[1404,7]])
+    track[np.arange(1396,1404+1),8] = np.interp(np.arange(1396,1404+1), [1396,1404], [track[1396,8], track[1404,8]])
     """
-    argmax_diff_disp = np.argmax(abs(np.diff(np.linalg.norm(track1[:, 7:9], axis=1))))
-    max_diff_disp = np.max(abs(np.diff(np.linalg.norm(track1[:, 7:9], axis=1))))
+    argmax_diff_disp = np.argmax(abs(np.diff(np.linalg.norm(track[:, 7:9], axis=1))))
+    max_diff_disp = np.max(abs(np.diff(np.linalg.norm(track[:, 7:9], axis=1))))
     if max_diff_disp > max_disp:
         ind1 = argmax_diff_disp - 4
         ind2 = argmax_diff_disp + 4
-        track1[np.arange(ind1, ind2 + 1), 7] = np.interp(
-            np.arange(ind1, ind2 + 1), [ind1, ind2], [track1[ind1, 7], track1[ind2, 7]]
+        track[np.arange(ind1, ind2 + 1), 7] = np.interp(
+            np.arange(ind1, ind2 + 1), [ind1, ind2], [track[ind1, 7], track[ind2, 7]]
         )
-        track1[np.arange(ind1, ind2 + 1), 8] = np.interp(
-            np.arange(ind1, ind2 + 1), [ind1, ind2], [track1[ind1, 8], track1[ind2, 8]]
+        track[np.arange(ind1, ind2 + 1), 8] = np.interp(
+            np.arange(ind1, ind2 + 1), [ind1, ind2], [track[ind1, 8], track[ind2, 8]]
         )
-    return track1, track2
+    return track
 
 
 def remove_static_tracks(tracks, window_size=16, move_threshold=10):
@@ -151,16 +151,19 @@ def make_new_detections_for_track(track, bboxes, start_frame, end_frame):
     detections = []
     for frame_number, bbox in zip(frame_numbers, bboxes):
         cent_wh = cen_wh_from_tl_br(*bbox)
-        detection = [frame_number, track_id, -1, *bbox, *cent_wh]  # TODO
+        bbox = np.int64(np.round(bbox, decimals=0))
+        cent_wh = np.int64(np.round(cent_wh, decimals=0))
+        detection = [track_id, frame_number, -1, *bbox, *cent_wh, -1]  # TODO
         detections.append(detection)
     new_track = np.concatenate((track, detections), axis=0)
+    new_track = new_track[new_track[:, 1].argsort()]
     return new_track
 
 
 def interpolate_track_when_missing_frames(track):
     frame_numbers = np.unique(track[:, 1])
     all_frame_numbers = np.arange(frame_numbers[0], frame_numbers[-1] + 1)
-    missing_frames = set(all_frame_numbers).difference(set(frame_numbers))
+    missing_frames = np.array(sorted(set(all_frame_numbers).difference(frame_numbers)))
     if len(missing_frames) == 0:
         return track
     new_track = track.copy()
@@ -176,7 +179,7 @@ def interpolate_track_when_missing_frames(track):
 
 
 def interpolate_tracks_when_missing_frames(tracks):
-    new_tracks = np.empty(shape=(0, 11), dtype=np.int64)
+    new_tracks = np.empty(shape=(0, tracks.shape[1]), dtype=np.int64)
     tracks_ids = np.unique(tracks[:, 0])
     for track_id in tracks_ids:
         track = tracks[tracks[:, 0] == track_id]  # copy
@@ -185,8 +188,52 @@ def interpolate_tracks_when_missing_frames(tracks):
     return new_tracks
 
 
-# remove static tracks, remove short tracks, interpolate tracks, correct outliers, reindexing
-# remove_short_tracks, interpolate_tracks_when_missing_frames, correct_outliers, reindex_tracks
+def tid_from_xyf(tracks, xcent, ycent, frame, thrs=1):
+    """
+    get track id from detection center x, y and frame number
+    """
+    mask = (
+        (abs(tracks[:, 7] - xcent) < thrs)
+        & (abs(tracks[:, 8] - ycent) < thrs)
+        & (tracks[:, 1] == frame)
+    )
+    if len(tracks[mask]) == 0:
+        return
+    track_id = tracks[mask][0, 0]
+    print(tracks[mask])
+    return track_id
+
+
+def merge_two_tracks_by_speed(track1, track2, thrs=6):
+    """Merge two tracks
+    Due to (partial) occlusions, there is id switchs.
+    E.g. groudtruth track 1 consists of track 6, 70
+    """
+    # check with track follows other track
+    if track2[-1, 1] < track1[0, 1]:
+        tmp = track2.copy()
+        track2 = track1.copy()
+        track1 = tmp
+    # normalized displacement (not a real speed)
+    disp = np.linalg.norm(track2[0, 7:9] - track1[-1, 7:9]) / (
+        track2[0, 1] - track1[-1, 1]
+    )
+    if disp < thrs:
+        tid = track1[0, 0]
+        track = np.concatenate((track1, track2), axis=0)  # copy
+        track[:, 0] = tid
+    return track
+
+
+def plot_2d_tracks(tracks):
+    track_ids = np.unique(tracks[:, 0])
+    plt.figure()
+    for tid in track_ids:
+        track = tracks[tracks[:, 0] == tid]
+        plt.plot(track[:, 7], track[:, 8], "-*", label=str(tid))
+    plt.gca().invert_yaxis()
+    plt.legend()
+
 
 # fmt: off
 gt_matches = {3:0, 5:1, 4:2, 2:3, 8:4, 0:5, 1:6, 6:7, 7:8, 9:9, 10:10, 11:11, 12:12, 13:13, 14:14}
@@ -204,44 +251,46 @@ tracks2 = da.load_tracks_from_mot_format(
     Path("/home/fatemeh/Downloads/fish/mot_data/ms_exp1/mots/129_2_ms_exp1.txt")
 )
 
-min_track_length = 16
+# gtrack1: 0->0,72,74 (16.76 0-72); 1->6,70 (6.1); 2->1; 3->7; 4->9; 5->4,51,81 (10.8 4-51); 6->3,71; 7->5,75 (8.3);
+# 8->2; 9->36; 10->55; 11->61; 12->67; 13->82; 14->84
+# track 2(larger disp 6.2,10.2)
+# [ 0,  1,  2,  3,  4,  5,  6,  7,  9, 36, 51, 55, 61, 67, 70, 71, 72, 74, 75, 81, 82, 84]
+gtrack1 = gtracks1[(gtracks1[:, 0] == 1)]
+for det in gtrack1[::16]:
+    tid_from_xyf(tracks1, det[7], det[8], det[1], thrs=5)
+track1 = tracks1[(tracks1[:, 0] == 6)]
+track2 = tracks1[(tracks1[:, 0] == 70)]
+
+
+plt.figure()
+plt.plot(gtrack1[:, 7], gtrack1[:, 8], "*-g")
+plt.plot(track1[:, 7], track1[:, 8], "o--")
+plt.plot(track2[:, 7], track2[:, 8], "o--")
+
+track = merge_two_tracks_by_speed(track1, track2, thrs=7)
+new_track = interpolate_track_when_missing_frames(track)
+
+disp1 = abs(np.diff(np.linalg.norm(track1[:, 7:9], axis=1))) / np.diff(track1[:, 1])
+disp2 = abs(np.diff(np.linalg.norm(track2[:, 7:9], axis=1))) / np.diff(track2[:, 1])
+plt.figure()
+plt.plot(track1[1:, 1], disp1, "*-")
+plt.plot(track2[1:, 1], disp2, "*-")
+
 track_lens1 = get_tracks_lengths(tracks1)
 sorted(track_lens1.items(), key=lambda x: x[1])
 
-tracks1_lt = remove_short_tracks(tracks1, min_track_length)
-track_lt_lens1 = get_tracks_lengths(tracks1_lt)
-tracks1_lt_ri = reindex_tracks(tracks1_lt)
 
-track1 = tracks1[tracks1[:, 0] == 7]
-plt.figure()
-plt.plot(track1[::16, 7], track1[::16, 8], "g-*")
-track1 = tracks1_lt[tracks1_lt[:, 0] == 7]
-plt.figure()
-plt.plot(track1[::16, 7], track1[::16, 8], "g-*")
-track1 = tracks1_lt_ri[tracks1_lt_ri[:, 0] == 7]
-plt.figure()
-plt.plot(track1[::16, 7], track1[::16, 8], "g-*")
+# remove static tracks, remove short tracks, split and merge tracks, reindexing, interpolate tracks, correct outliers (?)
+tracks = tracks1.copy()
+tracks = remove_static_tracks(tracks, window_size=16, move_threshold=10)
+tracks = remove_short_tracks(tracks, min_track_length=16)
+track = merge_two_tracks_by_speed(track1, track2, thrs=7)
+s_tracks = tracks[np.isin(tracks[:, 0], [0, 1, 2, 3, 4, 5, 6, 7, 9])]
+plot_2d_tracks(s_tracks)
+tracks = reindex_tracks(tracks)
+tracks = interpolate_tracks_when_missing_frames(tracks)
+plot_2d_tracks(tracks)
 
-
-def plot_2d_tracks(tracks):
-    track_ids = np.unique(tracks[:, 0])
-    plt.figure()
-    for tid in track_ids:
-        track = tracks[tracks[:, 0] == tid]
-        plt.plot(track[:, 7], track[:, 8], "-*", color="gray")
-
-
-sorted(get_tracks_lengths(tracks1_lt_ri).items(), key=lambda x: x[1])
-# track1 = tracks1_lt_ri[tracks1_lt_ri[:, 0] == 22]
-track_ids = [9, 10, 11, 13, 16, 18, 23, 26, 27]
-_ = remove_static_tracks(tracks1_lt_ri, window_size=16, move_threshold=10)
-plot_2d_tracks(tracks1_lt_ri)
-plt.figure()
-for track_id in track_ids:
-    track1 = tracks1_lt_ri[tracks1_lt_ri[:, 0] == track_id]
-    plt.plot(track1[:, 7], track1[:, 8], "-o", label=str(track_id))
-plt.legend()
-plt.show()
 
 # fmt: off
 # TODO: remove
@@ -250,7 +299,7 @@ gt_matches = {3:0, 5:1, 4:2, 2:3, 8:4, 0:5, 1:6, 6:7, 7:8, 9:9, 10:10, 11:11, 12
 # gt_matches = {3:0, 5:0, 4:0, 2:0, 8:0, 0:0, 1:0, 6:0, 7:0, 9:0, 10:0, 11:0, 12:0, 13:0, 14:0}
 
 track1, track2 = get_matching_frames_between_tracks(tracks1, tracks2, 7, 8)
-track1, track2 = correct_outliers(track1, track2)
+track1 = correct_outliers(track1)
 
 plt.figure();plt.plot(track1[::16,7],track1[::16,8],'g-*');plt.plot(track2[::16,7],track2[::16,8],'r-*');plt.show(block=False)
 # plt.figure();plt.plot(np.diff(np.linalg.norm(track1[:,7:9], axis=1)),'-*');plt.plot(np.diff(np.linalg.norm(track2[:,7:9], axis=1)),'-*');plt.title(f"{id1}:{id2}");plt.show(block=False)
